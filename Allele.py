8e6a83f8027964dec65cbdf0a22f76453dce1e31
deusxaviermachina <58574036+deusxaviermachina@users.noreply.github.com>
2020-11-15 15:22:03 -0500

    Add files via upload

---
+++ Allele.py
@@ -0,0 +1,108 @@
+import random
+
+class Allele:
+    def __init__(self,s,h,symbol):
+        self.s=s
+        self.h=h
+        """
+        DOMINANCE COEFFICIENT=H:
+        IF H = 1, then allele 'a' is fully dominant (meaning that Genotype Aa--->phenotype a 100% of the time).
+        If H=0, the opposite is true: "A" is fully dominant, so Genotype Aa--->phenotype A 100% of the time
+        if H=0.5, then Aa;s fitness is intermediate with respect to AA and aa. EG suppose
+        that S=0.75 and H=0.5. fitness(AA)=1, fitness(aa)=1-s=1-0.75=0.25,. 1-h(s)= 1-(0.5*0.75) = 1-0.375=0.625
+        So, we have aa=0.25, Aa=0.625, AA=1
+        
+        'HIGH DOMINANCE' IS ESSENTIALLY JUST 'THE DEGREE OF PHENOTYPIC SIMILARITY OF THE
+        'WEAK' HOMOZYGOTE (TYPICALLY REPRESENTED WITH LOWERCASE CHARACTERs) TO THE HETEROZYGOTE--
+        SO, A DOMINANCE SCORE OF "1" MEANS THE FOLLOWING:
+        
+        H=1 ---> [(AA--->A) & (Aa--->a) & (aa--->a)]   
+        
+        i.e. the phenotypic expression of the "aa" homozygote 
+        will be identical to that of the heterozygote
+        
+        A DOMINANCE SCORE OF "0" WOULD MEAN THE OPPOSITE, SO:
+        H=0 ---> [(AA--->A) & (Aa--->A) & (aa--->a)]
+        
+        i.e. the AA homozygote's phenotypic expression is no different from that of the "Aa" homozygote
+        
+        THESE TWO EXTREMES ARE THE BINARY MENDELIAN GENETICS THAT ARE TAUGHT TO MIDDLE-SCHOOLERS--
+        REALLY THOUGH, MOST TRAITS FOLLOW A CONTINUOUS LOGIC AND NOT A BINARY LOGIC--FOR EXAMPLE, TAKE HEIGHT:
+        THE CATEGORICAL BOUNDARY BETWEEN 'SHORT' AND 'TALL' IS NOT SHARP (unlike, for example, attached/detached
+        earlobes, or the widow's peak which are certainly bimodal (if not completely binary)). 
+        SAY FOR HEIGHT, THE DOMINANCE SCORE IS "0.5": THOSE WITH THE "TT" GENOTYPE WILL BE "TALL", THOSE WITH THE "tt"
+         WILL BE "SHORT", AND THOSE WITH genotype "Tt" WILL BE "INTERMEDIATE". 
+         tl:dr AS THE DOMINANCE SCORE APPROACHES 0.5, THE DISTRIBUTION BECOMES LESS
+        BIMODAL WITH RESPECT TO THE TRAIT IN QUESTION. AS IT APPROACHES 0 AND 1, IT APPROACHES A 
+        BERNOULLI/BINARY DISTRIBUTION
+        'WEAK' TRAIT CANCELS 'STRONG' TRAIT IN HETEROZYGOTE IF H=1
+        'STRONG' TRAIT CANCELS 'WEAK' TRAIT IN HEREROZYGOTE IF H=0
+        """
+        self.symbol=symbol.lower()
+
+    def gen_genotype_population(self,pop_size):
+        pop=[]
+        """
+        create a 'strong'("A") and a 'weak'("a")
+        allele variable.
+        """
+        allele1, allele2=self.symbol.upper(), self.symbol.lower()
+        AA=self.symbol.upper()*2
+        Aa=self.symbol.upper()+self.symbol.lower()
+        aa=self.symbol.lower()*2
+        AA_fitness=1
+        Aa_fitness=1-self.h*self.s
+        aa_fitness=1-self.s
+        population=random.choices([AA,Aa,aa], weights=[AA_fitness,Aa_fitness,aa_fitness],k=pop_size)
+        return population
+
+    def mate(self,pop_size,survival_to_death_ratio):
+        pop=self.gen_genotype_population(pop_size)
+        """
+        'culling of the weak' as it were--a fraction of the population survives into next generation,
+        hence the 'survival to death ratio'
+        """
+        new_pop=random.choices(pop, k=int(pop_size*survival_to_death_ratio))
+        [mate1, mate2] = [random.choice(pop), random.choice(pop)]
+        zygote = []
+        #this is similar to a Punnett square--a crossover/breeding event
+        for mate in [mate1, mate2]:
+            if mate == self.symbol.upper() * 2:
+                zygote.append(self.symbol.upper())
+            elif mate == self.symbol.lower() * 2:
+                zygote.append(self.symbol.lower())
+            else:
+                zygote.append(random.choices([self.symbol.upper(), self.symbol.lower()],
+                                             weights=(1 - self.s * self.h, 1 - self.s), k=1)[0])
+            print(zygote)
+        if [zygote[0], zygote[1]] == [self.symbol.lower(), self.symbol.upper()]:
+            [zygote[0], zygote[1]] = [zygote[1], zygote[0]]
+            print("updated: ", zygote, ('').join(zygote))
+
+        for i in range(int(survival_to_death_ratio*pop_size)):
+            new_pop.append(('').join(zygote))
+        return "New Population: ",new_pop
+
+if __name__ == "__main__":
+    pop_size=100
+    pop=Allele(0,0.3, "a").gen_genotype_population(pop_size)
+    count_AA=0
+    count_Aa=0
+    count_aa=0
+    for i in pop:
+        if i =="AA":
+            count_AA+=1
+        elif i =="Aa":
+            count_Aa+=1
+        else:
+            count_aa+=1
+    print(" Count of AA Genotype:",count_AA,"\n","Count of Aa Genotype:",count_Aa,"\n","Count of aa genotype:",count_aa)
+    print("INITIAL POPULATION: ","\n",pop)
+    survival_to_death_ratio=0.5
+    #proportion of population that survives to mating age
+    new_pop=random.choices(pop,k=int(len(pop)*survival_to_death_ratio))
+    print("\n","New Mating Pool After Selection Pressures Applied","\n",new_pop)
+    AA=new_pop.count("AA")
+    Aa=new_pop.count("Aa")
+    aa=new_pop.count('aa')
+    print("Freq of genotypes in new mating pool:","\n", "AA: ", AA,"\n","Aa: ",Aa, "\n","aa: ",aa)
